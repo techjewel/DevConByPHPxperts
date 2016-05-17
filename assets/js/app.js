0f8f44d0fcb7f99cb87a84c9d89365fbb7f84cdc
Shahjahan Jewel <cep.jewel@gmail.com>
2016-05-17 16:38:45 +0600

    Smooth Scrolling Internal Links and Buy now button fix
    - Smooth Scrolling Internal Links
    - Buy Ticket internal links fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -55,3 +55,38 @@ jQuery(document).ready(function($){
     });
 
 });
+
+
+
+
+// jQuery Smooth Scrolling Internal Links
+// credit: https://gist.github.com/nathos/2511377
+jQuery(function($) {
+    var scrollElement = 'html, body';
+    $('html, body').each(function () {
+        var initScrollTop = $(this).attr('scrollTop');
+        $(this).attr('scrollTop', initScrollTop + 1);
+        if ($(this).attr('scrollTop') == initScrollTop + 1) {
+            scrollElement = this.nodeName.toLowerCase();
+            $(this).attr('scrollTop', initScrollTop);
+            return false;
+        }
+    });
+
+    // Smooth scrolling for internal links
+    $("a[href^='#']").click(function(event) {
+        event.preventDefault();
+
+        var $this = $(this),
+            target = this.hash,
+            $target = $(target);
+
+        $(scrollElement).stop().animate({
+            'scrollTop': $target.offset().top - 50
+        }, 300, 'swing', function() {
+            window.location.hash = target;
+        });
+
+    });
+
+});
